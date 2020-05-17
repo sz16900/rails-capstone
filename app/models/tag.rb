b948e817b52509c5bf35676969ad2ab9a304b4b5
sz16900 <sz16900@my.bristol.ac.uk>
2020-05-17 17:48:24 -0500

    votes cannot have duplicaes

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
     belongs_to :article
     belongs_to :category
+    
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,14 @@ class Vote < ApplicationRecord
     belongs_to :user
     belongs_to :article
 
-
+    validate :vote_valid?
+  
+    private
+    def vote_valid?
+      if Vote.where(user_id: user_id, article_id: article_id).exists? ||
+         Vote.where(user_id: article_id, article_id: user_id).exists? ||
+         user_id == article_id
+        errors.add(:user_id, "cannot have duplicate id's")
+      end
+    end
 end
