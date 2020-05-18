514196006ecdeba09b6cbe62b3d8816593cd05c0
sz16900 <sz16900@my.bristol.ac.uk>
2020-05-18 14:11:57 -0500

    working with partials

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,11 +7,6 @@ class Category < ApplicationRecord
     def self.top_priority
         # And this is how you avoid n+1 problems
         Category.order("priority ASC").limit(4).includes(:articles)
-        # categories.each do |cat|
-        #     p cat.name
-        #     p cat.articles.last.title
-        #     p cat.articles.last.text.truncate(50)
-        # end
     end
 
 end
