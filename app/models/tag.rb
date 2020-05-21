fbff30cd29fea9acec3307ea16bed413afd671ca
Seth Zea <zea.seth@gmail.com>
2020-05-20 20:24:48 -0500

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -12,8 +12,9 @@ class Tag < ApplicationRecord
 
   def tag_valid?
     if Tag.where(category_id: category_id, article_id: article_id).exists?
-      return errors.add(:category_id, "cannot have duplicate id's")
+      errors.add(:category_id, "cannot have duplicate id's")
+    else
+      return true
     end
   end
-  true
 end
