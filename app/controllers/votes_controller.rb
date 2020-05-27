5ff0401611d9d6f487440b00aeebc71c0264d591
sz16900 <sz16900@my.bristol.ac.uk>
2020-05-27 13:28:36 -0500

    fixed footer

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,12 @@
 class VotesController < ApplicationController
   before_action :find_article
 
-  def create
+  def create 
     @vote = current_user.votes.new(article_id: params[:article_id])
     if @vote.save
-      redirect_to category_path, notice: 'You voted an article.'
+      redirect_to root_path, notice: 'You voted an article.'
     else
-      redirect_to category_path, alert: 'You cannot vote this post.'
+      redirect_to root_path, alert: 'You cannot vote this post.'
     end
   end
 
@@ -14,9 +14,9 @@ class VotesController < ApplicationController
     vote = Vote.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
     if vote
       vote.destroy
-      redirect_to category_path, notice: 'You disvoted an article.'
+      redirect_to root_path, notice: 'You disvoted an article.'
     else
-      redirect_to category_path, alert: 'You cannot disvote article that you did not vote before.'
+      redirect_to root_path, alert: 'You cannot disvote article that you did not vote before.'
     end
   end
 
